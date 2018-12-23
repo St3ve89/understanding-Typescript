3abb18f25caca2312c1e2bf88c89a129e4f57753
Istvan Acs <istvan.acs89@gmail.com>
2018-12-23 18:12:51 +0000

    The "any" type

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ var Color;
 (function (Color) {
     Color[Color["Gray"] = 0] = "Gray";
     Color[Color["Green"] = 100] = "Green";
-    Color[Color["Blue"] = 101] = "Blue"; // 2
+    Color[Color["Blue"] = 2] = "Blue"; // 2
 })(Color || (Color = {}));
 var myColor = Color.Blue;
 console.log(myColor);
+// any 
+var car = 'BMW';
+console.log(car);
+car = { brand: 'BMW', series: 3 };
+console.log(car);
--- app.ts
+++ app.ts
@@ -31,3 +31,10 @@ enum Color {
 }
 let myColor: Color = Color.Blue
 console.log(myColor)
+
+
+// any 
+let car: any = 'BMW'
+console.log(car)
+car = {brand: 'BMW', series: 3}
+console.log(car)
