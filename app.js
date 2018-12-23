5593e2c3fa287a34702444b299a6cfd20faa2039
Istvan Acs <istvan.acs89@gmail.com>
2018-12-23 17:48:29 +0000

    Type basics

---
+++ app.js
@@ -0,0 +1,9 @@
+//  string
+var myName = 'Istvan';
+myName = 28;
+//  number 
+var age = 29;
+// age = 'Istvan'
+// boolean
+var hasHobbies = true;
+hasHobbies = 1;
---
+++ app.ts
@@ -0,0 +1,11 @@
+//  string
+let myName = 'Istvan'
+// myName = 28
+
+//  number 
+let age = 29;
+// age = 'Istvan'
+
+// boolean
+let hasHobbies = true
+// hasHobbies = 1
