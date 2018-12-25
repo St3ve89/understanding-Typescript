e2cbb4858725b5a64bb227232d7db9d49bf272ea
Istvan Acs <istvan.acs89@gmail.com>
2018-12-25 10:19:58 +0000

    Putting it all together in a complex object

--- app.js
+++ app.js
@@ -60,3 +60,10 @@ var userData = {
 //   a: 'Hello',
 //   b: 22
 // }
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
--- app.ts
+++ app.ts
@@ -74,3 +74,14 @@ let userData: { name: string,age: number } = {
 //   a: 'Hello',
 //   b: 22
 // }
+
+// complex object
+let complex: {data: number[], output: (all: boolean) => number[]} = {
+  data: [100, 3.99, 10],
+
+  output: function (all: boolean): number[] {
+    return this.data;
+  }
+};
+
+// complex = {};
