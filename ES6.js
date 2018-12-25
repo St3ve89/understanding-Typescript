b0d1e8577235a82edad3c8a9318fb315551f96b8
Istvan Acs <istvan.acs89@gmail.com>
2018-12-25 12:29:34 +0000

    Let and const

---
+++ ES6.js
@@ -0,0 +1,8 @@
+// let & const 
+var variable = "Test";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+var maxLevel = 100;
+console.log(maxLevel);
+// maxLevel = 99; Won't work
---
+++ ES6.ts
@@ -0,0 +1,9 @@
+// let & const 
+let variable = "Test";
+console.log(variable);
+variable = "Another value"
+console.log(variable);
+
+const maxLevel = 100;
+console.log(maxLevel)
+// maxLevel = 99; Won't work
