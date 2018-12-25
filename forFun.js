9e698f847869bd53d1620c3d410fbc6e1cd0c1fe
Istvan Acs <istvan.acs89@gmail.com>
2018-12-25 12:11:35 +0000

    Compiler improvements

---
+++ forFun.js
@@ -0,0 +1,13 @@
+var myname = 'Istvan';
+var mayAge = 29;
+var anything;
+anything = 12;
+// myName = 30
+function controlMe(isTrue, somethingElse) {
+    var result;
+    if (isTrue) {
+        result = 12;
+    }
+    result = 33;
+    return result;
+}
---
+++ forFun.ts
@@ -0,0 +1,17 @@
+let myname: string ='Istvan';
+let mayAge: number = 29;
+let anything;
+
+anything = 12;
+
+// myName = 30
+
+
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+  let result: number;
+  if(isTrue) {
+    result = 12;
+  }
+  result = 33;
+  return result;
+}
