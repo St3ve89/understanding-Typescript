35a185a97ed01dcb63df0adb7ddfe42182e01e4e
Istvan Acs <istvan.acs89@gmail.com>
2018-12-26 15:52:10 +0000

    Namespaces and multiple files

--- namespaces-modules.js
+++ namespaces-modules.js
@@ -1,15 +1,3 @@
-var MyMath;
-(function (MyMath) {
-    var PI = 3.14;
-    function calculateCircumference(diameter) {
-        return diameter * PI;
-    }
-    MyMath.calculateCircumference = calculateCircumference;
-    function calculateRectangle(width, length) {
-        return width * length;
-    }
-    MyMath.calculateRectangle = calculateRectangle;
-})(MyMath || (MyMath = {}));
 var PI = 3.12;
 console.log(MyMath.calculateRectangle(10, 20));
 console.log(MyMath.calculateCircumference(3));
--- namespaces-modules.ts
+++ namespaces-modules.ts
@@ -1,15 +1,3 @@
-namespace MyMath {
-  const PI = 3.14;
-  
-  export function calculateCircumference(diameter: number) {
-    return diameter * PI;
-  }
-  
-  export function calculateRectangle(width: number, length: number) {
-    return width * length;
-  }
-}
-
 const PI = 3.12;
 
 console.log(MyMath.calculateRectangle(10,20));
