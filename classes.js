55aca53754fb0580370e3981bd0faba783510242
Istvan Acs <istvan.acs89@gmail.com>
2018-12-26 10:37:10 +0000

    Class methods and access modifiers

--- classes.js
+++ classes.js
@@ -1,9 +1,20 @@
 var Person = /** @class */ (function () {
     function Person(name, username) {
         this.username = username;
+        this.age = 29;
         this.name = name;
     }
+    Person.prototype.printAge = function () {
+        console.log(this.age);
+        this.setType('Old guy!');
+    };
+    Person.prototype.setType = function (type) {
+        this.type = type;
+        console.log(this.type);
+    };
     return Person;
 }());
 var person = new Person("Istvan", "steve");
 console.log(person.name, person.username);
+person.printAge();
+// person.setType('Cool guy');  Won't work with private method
--- classes.ts
+++ classes.ts
@@ -1,12 +1,24 @@
 class Person {
   name: string;
   private type: string;
-  protected age: number;
+  protected age: number = 29;
 
   constructor(name: string, public username: string) {
     this.name = name;
+  }
+
+  printAge() {
+    console.log(this.age)
+    this.setType('Old guy!')
+  }
+
+  private setType(type: string) {
+    this.type = type
+    console.log(this.type)
   }
 }
 
 const person = new Person("Istvan", "steve");
 console.log(person.name, person.username)
+person.printAge();
+// person.setType('Cool guy');  Won't work with private method
