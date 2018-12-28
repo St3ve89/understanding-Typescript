5bc9b8434af3cb094a41340e9551bbb8adc8315e
Istvan Acs <istvan.acs89@gmail.com>
2018-12-28 11:36:47 +0000

    Built-in generic type: array

--- generics.js
+++ generics.js
@@ -12,3 +12,8 @@ function betterEcho(data) {
 console.log(betterEcho('Istvan').length);
 console.log(betterEcho(29));
 console.log(betterEcho({ name: 'Istvan', age: 29 }));
+// built-in generics
+var testResult = [1.94, 2.33];
+testResult.push(-2.99);
+// testResult.push("string");   Won't work
+console.log(testResult);
--- generics.ts
+++ generics.ts
@@ -15,3 +15,9 @@ function betterEcho<T>(data: T) {
 console.log(betterEcho('Istvan').length);
 console.log(betterEcho<number>(29));
 console.log(betterEcho({name: 'Istvan', age: 29}));
+
+// built-in generics
+const testResult: Array<number> =  [1.94 , 2.33];
+testResult.push(-2.99);
+// testResult.push("string");   Won't work
+console.log(testResult);
