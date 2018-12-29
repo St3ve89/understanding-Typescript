5b3e1445cba0d3a7162571f5ab8332e39c5784d7
Istvan Acs <istvan.acs89@gmail.com>
2018-12-29 19:29:34 +0000

    Parameter decorator

--- decorator.js
+++ decorator.js
@@ -4,6 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logged(constructorFn) {
     console.log(constructorFn);
 }
@@ -81,3 +84,29 @@ project.calcBudget = function () {
 };
 project.calcBudget();
 console.log(project);
+// Parameter decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log("Target: ", target);
+    console.log("methodName: ", methodName);
+    console.log("paramIndex: ", paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers");
+    return Course;
+}());
+var course = new Course("Super course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
--- decorator.ts
+++ decorator.ts
@@ -70,3 +70,29 @@ project.calcBudget = function () {
 };
 project.calcBudget();
 console.log(project);
+
+// Parameter decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log("Target: ", target);
+  console.log("methodName: ", methodName);
+  console.log("paramIndex: ", paramIndex);
+}
+class Course {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  printStudentNumbers(mode: string, @printInfo printAll: boolean) {
+    if(printAll) {
+      console.log(10000);
+    } else {
+      console.log(2000);
+    }
+  }
+}
+
+const course = new Course("Super course");
+course.printStudentNumbers("anything", true);
+course.printStudentNumbers("anything", false);
