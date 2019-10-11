96f170d71ee9541d770c129450dd41a123cf7772
Istvan Acs <istvan.acs89@gmail.com>
2019-10-11 22:44:56 +0100

    Sorting implementation

--- sort/build/index.js
+++ sort/build/index.js
@@ -3,7 +3,21 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
-    Sorter.prototype.sort = function () { };
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // reference to the element on the left hand side
+                    var leftHand = this.collection[j];
+                    // take the right hand side and throw over to left
+                    this.collection[j] = this.collection[j + 1];
+                    // take left hand side and throw to the right
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
     return Sorter;
 }());
 var sorter = new Sorter([10, 3, -5, 0]);
