f5e59061d863ae2ef5c2562121d3960484e0d316
Istvan Acs <istvan.acs89@gmail.com>
2019-10-11 22:37:55 +0100

    Sorter scaffolding

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('hello');
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello');
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
