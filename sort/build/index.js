b45f3aaa42d415508780a027f42ed495bb67f8fc
Istvan Acs <istvan.acs89@gmail.com>
2019-10-12 10:24:05 +0100

    Add type guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -7,13 +7,21 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    // reference to the element on the left hand side
-                    var leftHand = this.collection[j];
-                    // take the right hand side and throw over to left
-                    this.collection[j] = this.collection[j + 1];
-                    // take left hand side and throw to the right
-                    this.collection[j + 1] = leftHand;
+                // if collection is an array of numbers
+                if (this.collection instanceof Array) {
+                    //type guard
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        // reference to the element on the left hand side
+                        var leftHand = this.collection[j];
+                        // take the right hand side and throw over to left
+                        this.collection[j] = this.collection[j + 1];
+                        // take left hand side and throw to the right
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // if collection is a string, do this logic instead:
+                if (typeof this.collection === 'string') {
+                    //type guard
                 }
             }
         }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,18 +1,27 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          // reference to the element on the left hand side
-          const leftHand = this.collection[j];
-          // take the right hand side and throw over to left
-          this.collection[j] = this.collection[j + 1];
-          // take left hand side and throw to the right
-          this.collection[j + 1] = leftHand;
+        // if collection is an array of numbers
+        if (this.collection instanceof Array) {
+          //type guard
+          if (this.collection[j] > this.collection[j + 1]) {
+            // reference to the element on the left hand side
+            const leftHand = this.collection[j];
+            // take the right hand side and throw over to left
+            this.collection[j] = this.collection[j + 1];
+            // take left hand side and throw to the right
+            this.collection[j + 1] = leftHand;
+          }
+        }
+
+        // if collection is a string, do this logic instead:
+        if (typeof this.collection === 'string') {
+          //type guard
         }
       }
     }
