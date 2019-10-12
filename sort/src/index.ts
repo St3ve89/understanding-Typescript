5086e07a278efbbd0410f55041e699974c05c24a
Istvan Acs <istvan.acs89@gmail.com>
2019-10-12 12:33:22 +0100

    Try linked list sorting

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,24 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './numbersCollection';
 import { CharactersCollection } from './charactersCollection';
+import { LinkedList } from './linkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
