67665be7bfab17d5635e29dac72113520741378e
Istvan Acs <istvan.acs89@gmail.com>
2019-10-12 11:30:24 +0100

    Sorting arbitrary collections

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var sorter_1 = require("./sorter");
-var numbersCollection_1 = require("./numbersCollection");
-var numbersCollection = new numbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-var sorter = new sorter_1.Sorter(numbersCollection);
+var charactersCollection_1 = require("./charactersCollection");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+var charactersCollection = new charactersCollection_1.CharactersCollection('Xaayb');
+var sorter = new sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './numbersCollection';
+import { CharactersCollection } from './charactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
