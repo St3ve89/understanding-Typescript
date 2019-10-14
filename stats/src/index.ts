2978f117e40b0f0a3fdd408f99c51eb47e43706e
Istvan Acs <istvan.acs89@gmail.com>
2019-10-14 17:07:47 +0100

    Extracting csv file reader

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,14 +1,7 @@
-import fs from 'fs';
+import { CsvFileReader } from './csvFileReader';
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
-
+const reader = new CsvFileReader('football.csv');
+reader.read();
 // enum - enumeration
 enum MatchResult {
   HomeWin = 'H',
@@ -18,7 +11,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
