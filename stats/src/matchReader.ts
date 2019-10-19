53611d7f9834077873c48f2d08b356bfdc46d5f2
Istvan Acs <istvan.acs89@gmail.com>
2019-10-19 11:29:19 +0100

    Extracting match reference

--- stats/src/matchReader.ts
+++ stats/src/matchReader.ts
@@ -1,3 +1,7 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './matchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 interface DataReader {
   read(): void;
   data: string[][];
@@ -5,4 +9,18 @@ interface DataReader {
 
 export class MatchReader {
   constructor(public reader: DataReader) {}
+
+  // .map(
+  //   (row: string[]): MatchData => {
+  //     return [
+  //       dateStringToDate(row[0]),
+  //       row[1],
+  //       row[2],
+  //       parseInt(row[3]),
+  //       parseInt(row[4]),
+  //       row[5] as MatchResult,
+  //       row[6]
+  //     ];
+  //   }
+  // );
 }
