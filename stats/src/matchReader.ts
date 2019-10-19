569017e97485dd81487d15efa72a0cdd8ddf7e27
Istvan Acs <istvan.acs89@gmail.com>
2019-10-19 11:32:14 +0100

    Transforming data

--- stats/src/matchReader.ts
+++ stats/src/matchReader.ts
@@ -8,19 +8,24 @@ interface DataReader {
 }
 
 export class MatchReader {
+  matches: MatchData[] = [];
+
   constructor(public reader: DataReader) {}
 
-  // .map(
-  //   (row: string[]): MatchData => {
-  //     return [
-  //       dateStringToDate(row[0]),
-  //       row[1],
-  //       row[2],
-  //       parseInt(row[3]),
-  //       parseInt(row[4]),
-  //       row[5] as MatchResult,
-  //       row[6]
-  //     ];
-  //   }
-  // );
+  load(): void {
+    this.reader.read();
+    this.matches = this.reader.data.map(
+      (row: string[]): MatchData => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
+  }
 }
